8dc7430616b547db2385a6aed6b86f217b8e2183
Danil S <danils03122003@gmail.com>
2022-04-07 08:26:32 +0000

    editing: 4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-package-YOUR-USERNAME-HERE",
+    name="example-package-Q4rD",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
